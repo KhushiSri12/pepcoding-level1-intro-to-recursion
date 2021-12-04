50fca16953bedb520f7ebaa5c51cb7c3616b64dc
Khushi <68914425+KhushiSri12@users.noreply.github.com>
2021-12-04 11:50:25 +0530

    Add files via upload

---
+++ print increasing.java
@@ -0,0 +1,27 @@
+/*
+PEPCODING LEVEL-1 INTRODUCTION TO RECURSION
+
+
+PRINT INCREASING 
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n==0){
+            return;
+        }
+        printIncreasing(n-1);
+        System.out.println(n);
+    }
+
+}
