88c3f88c6b5f11dd8b1d46d924693e7e0a104d12
Khushi <68914425+KhushiSri12@users.noreply.github.com>
2021-12-06 21:14:54 +0530

    Add files via upload

---
+++ print zigzag.java
@@ -0,0 +1,31 @@
+/*
+PEPCODING LEVEL-1 INTRODUCTION TO RECURSION
+
+
+PRINT ZIGZAG(EULER'S TREE)
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+    if(n==0){
+            return;
+        }
+    System.out.print(n+" ");
+    pzz(n-1);
+    System.out.print(n+" ");
+    pzz(n-1);
+    System.out.print(n+" ");
+    
+    }
+
+}
